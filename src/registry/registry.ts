82bf70e7840c0f55ca7fe8a7d427155dc70b7d4c
unknown <penelope.peronnet@edu.devinci.fr>
2024-02-29 18:51:04 +0100

    debut step 3

--- src/registry/registry.ts
+++ src/registry/registry.ts
@@ -1,8 +1,9 @@
 import bodyParser from "body-parser";
 import express, { Request, Response } from "express";
 import { REGISTRY_PORT } from "../config";
+import { generateRsaKeyPair, exportPrvKey } from "../crypto";
 
-export type Node = { nodeId: number; pubKey: string };
+export type Node = { nodeId: number; pubKey: string; privateKey?: string };
 
 export type RegisterNodeBody = {
   nodeId: number;
@@ -10,13 +11,40 @@ export type RegisterNodeBody = {
 };
 
 export type GetNodeRegistryBody = {
-  nodes: Node[];
+  nodes: (Node & { privateKey?: string })[];
 };
+
+const registeredNodes: Node[] = [];
 
 export async function launchRegistry() {
   const _registry = express();
   _registry.use(express.json());
   _registry.use(bodyParser.json());
+
+  _registry.post('/registerNode', (req, res) => {
+    const { nodeId, pubKey }: RegisterNodeBody = req.body;
+
+    // Check if the node is already registered
+    const existingNode = registeredNodes.find((node) => node.nodeId === nodeId);
+
+    if (existingNode) {
+      return res.status(400).json({ message: `Node ${nodeId} is already registered.` });
+    }
+
+    // Add the node to the registered nodes array
+    registeredNodes.push({ nodeId, pubKey });
+
+     return res.status(201).json({ message: `Node ${nodeId} successfully registered.` });
+  });
+
+  _registry.get('/getNodeRegistry', (req, res) => {
+    const nodeRegistry: GetNodeRegistryBody = { nodes: registeredNodes };
+    res.json(nodeRegistry);
+  });
+
+  
+
+
 
   // TODO implement the status route
    _registry.get("/status", (req, res) => { res.send('live');});
@@ -26,4 +54,4 @@ export async function launchRegistry() {
   });
 
   return server;
-}
+} 
