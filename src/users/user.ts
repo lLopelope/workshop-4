f6f13af771ed2ab5b65bd304c7e4a163708aa3ec
unknown <penelope.peronnet@edu.devinci.fr>
2024-03-01 21:49:55 +0100

    step 4 debut

--- src/users/user.ts
+++ src/users/user.ts
@@ -1,6 +1,8 @@
 import bodyParser from "body-parser";
 import express from "express";
 import { BASE_USER_PORT } from "../config";
+import crypto from 'crypto';
+
 
 let lastReceivedMessage: string | null = null;
 let lastSentMessage: string | null = null;
@@ -19,12 +21,22 @@ export async function user(userId: number) {
    _user.get("/status", (req, res) => { res.send('live');});
 
    _user.get('/getLastReceivedMessage', (req, res) => {
-    res.json({ result: lastReceivedMessage });
+    res.json({ result : lastReceivedMessage });
 });
 
 // Route to get the last sent message
   _user.get('/getLastSentMessage', (req, res) => {
     res.json({ result: lastSentMessage });
+});
+
+_user.post("/message", (req, res) => {
+  const { message }: { message: string } = req.body;
+
+  // Update the last received message
+  lastReceivedMessage = message;
+  
+ res.send('success');
+  //res.json({ message: "Message received successfully" });
 });
 
   const server = _user.listen(BASE_USER_PORT + userId, () => {
